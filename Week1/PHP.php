d8400043c2543c37519a7e5d657925bd01f3add2
cspeloso <cspeloso@email.neit.edu>
2018-03-28 10:59:30 -0400

    third commit

--- Week1/PHP.php
+++ Week1/PHP.php
@@ -1,6 +1,27 @@
 <?PHP
 	
 	$size = mt_rand(5,10);
-	echo $size;
 	
+	$table = "<table>\n";
+	for($rows = 1;$rows <= $size;$rows++)
+	{
+		$table .= "\t<tr>";
+		for($cols = 1; $cols <= $size; $cols++)
+		{
+			$table .= "<td>" . $rows*$cols . "</td>";
+		}
+		$table .= "\t</tr>";
+	}
+	$table .= "</table>\n";
+	echo $table;
 ?>
+
+<!DOCTYPE html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Multiplication Table</title>
+</head>
+<body>
+<?php echo $table; ?>
+</body>
+</html>
